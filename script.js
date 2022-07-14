137b9648443cfd2d03bd601a1f1ece96d2111c62
Adrian Aguirre Barrionuevo <83303313+adragui@users.noreply.github.com>
2022-07-14 21:24:12 +0000

    Fix on Js and added copy option

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-const generateButton = document.getElementById("generate-btn")
+let savedPass = []
 let pwdShowOne = document.getElementById("pswd-show-one")
 let pwdShowTwo = document.getElementById("pswd-show-two")
 
-function randomValue () {
-    return Math.floor(Math.random()*characters.length)
+function passwordCreator() {
+    passArray = []
+    for (let i = 0 ; i < 15 ; i++) {
+        passArray.push(characters[Math.floor(Math.random()*characters.length)])
+    } 
+    return passArray.join("")
 }
 
-generateButton.addEventListener ("click", function() {
-    pwdShowOne.textContent = ""
-    pwdShowTwo.textContent = ""
-    for (let i = 0 ; i < 15 ; i++) {
-        pwdShowOne.textContent += characters[randomValue()]
-        pwdShowTwo.textContent += characters[randomValue()]
-    }
-})
+function pwdButton() {
+    savedPass = []
+    pwdShowOne.textContent = passwordCreator()
+    pwdShowTwo.textContent = passwordCreator()
+    savedPass.push(pwdShowOne.textContent, pwdShowTwo.textContent)
+}
+
+function copyToClipboard(text) {
+   const elem = document.createElement('textarea');
+   elem.value = text;
+   document.body.appendChild(elem);
+   elem.select();
+   document.execCommand('copy');
+   document.body.removeChild(elem);
+   alert("Se copio al Portapapeles")
+}
